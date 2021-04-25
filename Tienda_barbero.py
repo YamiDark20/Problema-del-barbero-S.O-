82194d0b75e66ea6b9ea40a25bfac2112064e4f3
YamiDark20 <caosyami27@gmail.com>
2021-04-25 17:00:09 -0400

    Version del programa en consola mejorado

--- Tienda_barbero.py
+++ Tienda_barbero.py
@@ -1,6 +1,6 @@
 import threading
 import time
-#n_sem = 1
+
 max_cap = threading.Semaphore(10)
 sofa = threading.Semaphore(4)
 silla_barbero = threading.Semaphore(3)
@@ -27,127 +27,239 @@ trab_silla1 = None
 trab_silla2 = None
 trab_silla3 = None
 
+# Tablas vacias donde se van a mostrar los datos
+Tabla = """\
++---------------------------------------------------------------------+
+| N° Cliente| Silla_B| Sofa| Area_Esp| Dentro_Shop| Fuera_Shop| Cajero|
+|---------------------------------------------------------------------|
+{}
++---------------------------------------------------------------------+\
+"""
+Tabla2 = """\
++-----------------------+
+| N° Cliente| N° Barbero|
+|-----------------------|
+{}
++-----------------------+\
+"""
+Tabla3 = """\
++---------------------------------------------+
+| N° Cliente| N° Barbero| Cortando| Finalizado|
+|---------------------------------------------|
+{}
++---------------------------------------------+\
+"""
+Tabla4 = """\
++-------------------------------------+
+| N° Cliente| Pagando| Recibo Recibido|
+|-------------------------------------|
+{}
++-------------------------------------+\
+"""
+
+#sillasActivas = [["N° Cliente", "N° Barbero"], [- 1, 0], [- 1, 1], [- 1, 2]]
+sillasActivas2 = [[- 1, 0], [- 1, 1], [- 1, 2]]
+
 
 class Cliente(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
         self.numcliente = 0
+        self.info2 = [[- 1, "No", "No", "No", "No", "Si", "No"]]
 
     def run(self):
-        global count, prueba, trab_silla1, trab_silla2, trab_silla3
+        global count, prueba, trab_silla1, trab_silla2, trab_silla3, Tabla
         max_cap.acquire()
-        print("Entrar a la tienda")
         time.sleep(3)
         mutex1.acquire()
         self.numcliente = count
+        self.info2[0][0] = self.numcliente
+        self.info2[0][3] = "Si"
+        self.info2[0][4] = "Si"
+        self.info2[0][5] = "No"
+        #Entrando a la tienda
+        print(Tabla.format('\n'.join(
+        "| {:>10}|{:>8}|{:>5}|{:>9}|{:>12}|{:>11}|{:>7}|".format(
+        *fila) for fila in self.info2)))
+
         count += 1
         mutex1.release()
         sofa.acquire()
-        print("Sentarse en sofa", self.numcliente)
+        self.info2[0][2] = "Si"
+        self.info2[0][3] = "No"
+        #Sentandose en el sofa
+        print(Tabla.format('\n'.join(
+        "| {:>10}|{:>8}|{:>5}|{:>9}|{:>12}|{:>11}|{:>7}|".format(
+        *fila) for fila in self.info2)))
         time.sleep(3)
         silla_barbero.acquire()
-        print("Levantarse del sofa", self.numcliente)
+
+        self.info2[0][2] = "No"
+        #Levantandose del sofa
+        print(Tabla.format('\n'.join(
+        "| {:>10}|{:>8}|{:>5}|{:>9}|{:>12}|{:>11}|{:>7}|".format(
+        *fila) for fila in self.info2)))
         time.sleep(4)
         sofa.release()
 
-        print("Sentarse en silla de barbero", self.numcliente)
+        self.info2[0][1] = "Si"
+        #Sentandose en la silla
+        print(Tabla.format('\n'.join(
+        "| {:>10}|{:>8}|{:>5}|{:>9}|{:>12}|{:>11}|{:>7}|".format(
+        *fila) for fila in self.info2)))
         time.sleep(5)
         mutex2.acquire()
-        print("Cliente ", self.numcliente, "en cola")
-        colaCorte.append(self.numcliente)
+        if(self.numcliente < 2):
+            time.sleep(1)
+        colaCorte.append(self.numcliente)  # ########
         cliente_listo.release()
         mutex2.release()
         terminado[self.numcliente].acquire()
-        print("Dejar silla del barbero", self.numcliente)
 
         #Las siguientes 8 lineas de codigo han sido modificada para
         #realizar lo pedido en la b)
         valorS = - 1
-        if(trab_silla1[1] == 0 and trab_silla1[0] == self.numcliente):
-            valorS = 0
-        elif(trab_silla2[1] == 1 and trab_silla2[0] == self.numcliente):
-            valorS = 1
-        elif(trab_silla3[1] == 2 and trab_silla3[0] == self.numcliente):
-            valorS = 2
-        print(valorS)
+        if(trab_silla1 is not None):
+            if(trab_silla1[1] == 0 and trab_silla1[0] == self.numcliente):
+                valorS = 0
+        if(trab_silla2 is not None):
+            if(trab_silla2[1] == 1 and trab_silla2[0] == self.numcliente):
+                valorS = 1
+        if(trab_silla3 is not None):
+            if(trab_silla3[1] == 2 and trab_silla3[0] == self.numcliente):
+                valorS = 2
         #Las siguientes lineas de codigo del if hasta el time.sleep
         #Las realice para simular que la lentintud de los primeros
         #procesos
         if(prueba > 0):
             prueba -= 10
         time.sleep(2 + prueba)
-        ############################################
-        #La siguiente linea de codigo la realice para saber el nombre del
-        #proceso que esta en curso
-        print(threading.current_thread().getName())
 
         #La siguiente linea es parte de lo que nos pide en la b)
         dejar_silla_b[valorS].release()
-        print("Pagar cuenta", threading.current_thread().getName(), self.numcliente)
+        self.info2[0][1] = "No"
+        self.info2[0][6] = "Si"
+        #Dejando silla y llendo hacia el cajero
+        print(Tabla.format('\n'.join(
+        "| {:>10}|{:>8}|{:>5}|{:>9}|{:>12}|{:>11}|{:>7}|".format(
+        *fila) for fila in self.info2)))
+        if (sillasActivas2[0][0] == self.numcliente):
+                sillasActivas2[0][0] = - 1
+        elif(sillasActivas2[1][0] == self.numcliente):
+                sillasActivas2[1][0] = - 1
+        elif(sillasActivas2[2][0] == self.numcliente):
+                sillasActivas2[2][0] = - 1
+        #Tabla de que barbero esta con que cliente
+        print(Tabla2.format('\n'.join(
+        "| {:>10}|{:>11}|".format(*fila) for fila in sillasActivas2)))
         time.sleep(7)
 
         realiza_pago.acquire()
         colaPago.append(self.numcliente)
         pago.release()
         recibo.acquire()
-        print("Salir de la tienda", threading.current_thread().getName())
-        print("\n\n\n")
-        #time.sleep(3)
+        self.info2[0][5] = "Si"
+        self.info2[0][6] = "No"
+        self.info2[0][4] = "No"
+        #Saliendo de la tienda
+        print(Tabla.format('\n'.join(
+        "| {:>10}|{:>8}|{:>5}|{:>9}|{:>12}|{:>11}|{:>7}|".format(
+        *fila) for fila in self.info2)))
         max_cap.release()
+
 
 class Barbero(threading.Thread):
     def __init__(self, index):
         threading.Thread.__init__(self)
         self.i = index
+        self.info = [
+            [- 1, - 1, "No", "No"]]
 
     def run(self):
         while(True):
             global trab_silla1, trab_silla2, trab_silla3
             cliente_listo.acquire()
             mutex2.acquire()
-            aux = colaCorte.pop()
-            print("Agregar en cola a cliente", aux, threading.current_thread().getName())
+            self.info[0][0] = colaCorte.pop()
+            self.info[0][1] = self.i
+            if(sillasActivas2[0][1] == self.i):
+                sillasActivas2[0][0] = self.info[0][0]
+            elif(sillasActivas2[1][1] == self.i):
+                sillasActivas2[1][0] = self.info[0][0]
+            elif(sillasActivas2[2][1] == self.i):
+                sillasActivas2[2][0] = self.info[0][0]
+
+            #Preparandose para cortale el pelo al cliente i
+            print(Tabla3.format('\n'.join(
+            "| {:>10}|{:>11}|{:>9}|{:>11}|".format(
+            *fila) for fila in self.info)))
+            #Tabla donde se coloca que barbero esta con que cliente
+            print(Tabla2.format('\n'.join(
+        "| {:>10}|{:>11}|".format(*fila) for fila in sillasActivas2)))
+
             mutex2.release()
             coord.acquire()
-            print("Cortando el pelo del cliente", aux)
+            self.info[0][2] = "Si"
+            #Cortandole el pelo al cliente i
+            print(Tabla3.format('\n'.join(
+            "| {:>10}|{:>11}|{:>9}|{:>11}|".format(
+            *fila) for fila in self.info)))
+
             time.sleep(4)
             coord.release()
-
-            #La siguiente linea de codigo la realice para saber el nombre del
-            #proceso que esta en curso
-            print(threading.current_thread().getName(), self.i)
 
             #Las siguientes 8 lineas de codigo han sido modificada para
             #realizar lo pedido en la b)
             mutex3.acquire()
             if(self.i == 0):
-                trab_silla1 = [aux, self.i]
+                trab_silla1 = [self.info[0][0], self.i]
             elif(self.i == 1):
-                trab_silla2 = [aux, self.i]
+                trab_silla2 = [self.info[0][0], self.i]
             elif(self.i == 2):
-                trab_silla3 = [aux, self.i]
+                trab_silla3 = [self.info[0][0], self.i]
             mutex3.release()
-            terminado[aux].release()
+            self.info[0][2] = "No"
+            self.info[0][3] = "Si"
+            #Corte de pelo del cliente i finalizado
+            print(Tabla3.format('\n'.join(
+            "| {:>10}|{:>11}|{:>9}|{:>11}|".format(
+            *fila) for fila in self.info)))
+            terminado[self.info[0][0]].release()
             dejar_silla_b[self.i].acquire()
-            ######################################################
             silla_barbero.release()
-            print("Corte finalizado del cliente", aux)
+            self.info[0][2] = "No"
+            self.info[0][3] = "No"
+            #print("Corte finalizado del cliente", self.info[0][0])
+
 
 class Cajero(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
+        self.info = [
+            [- 1, "No", "No"]]
 
     def run(self):
         while(True):
             pago.acquire()
             coord.acquire()
-            print("Realizando pago del cliente", colaPago.pop())
+            self.info[0][0] = colaPago.pop()
+            self.info[0][1] = "Si"
+            #Realizando el pago del cliente i
+            print(Tabla4.format('\n'.join(
+            "| {:>10}|{:>8}|{:>16}|".format(*fila) for fila in self.info)))
             time.sleep(4)
             coord.release()
             recibo.release()
+            self.info[0][1] = "No"
+            self.info[0][2] = "Si"
+            #Recibo entregado al cliente i
+            print(Tabla4.format('\n'.join(
+            "| {:>10}|{:>8}|{:>16}|".format(*fila) for fila in self.info)))
             realiza_pago.release()
+            self.info[0][1] = "No"
+            self.info[0][2] = "No"
 
-clientes = [Cliente() for _ in range(10)]
+clientes = [Cliente() for _ in range(14)]
 barbero = [Barbero(i) for i in range(3)]
 cajero = Cajero()
 
@@ -158,5 +270,3 @@ for b in barbero:
     b.start()
 
 cajero.start()
-print("Todo esta iniciado")
-#https://www.youtube.com/watch?v=lsHCzboWK0U
